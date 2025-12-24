0b4e5aeaf09d6850a43d9f58eba137d18e0c611d
navya <navyasoma683@gmail.com>
2025-12-24 15:13:33 +0530

    changes done

--- src/main/java/com/example/job/security/SecurityConfig.java
+++ src/main/java/com/example/job/security/SecurityConfig.java
@@ -68,7 +68,8 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
-        config.setAllowedOrigins(List.of("http://localhost:5173"));
+//        config.setAllowedOrigins(List.of("http://localhost:5173"));
+        config.setAllowedOrigins(List.of("http://jobportal-project-nu.vercel.app"));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         config.setAllowedHeaders(List.of("*"));
         config.setAllowCredentials(true);
--- src/main/java/com/example/job/security/WebConfig.java
+++ src/main/java/com/example/job/security/WebConfig.java
@@ -14,7 +14,8 @@ public class WebConfig {
             @Override 
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:5173")
+                       .allowedOrigins("http://jobportal-project-nu.vercel.app")
+//                        .allowedOrigins("http://localhost:5173")
                         .allowedMethods("GET", "POST", "PUT", "DELETE")
                         .allowedHeaders("*");
             }
